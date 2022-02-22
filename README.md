a6165930c797b513ebd80daf8948ade0989c8091
gracessong <gracesongpt@gmail.com>
2022-02-21 20:49:14 -0700

    version 1.0 of a simple calculator

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # calculator
+The good old simple calculator. Style inspired by Iphone's calculator app. Takes the number input from button clicks and voila, returns the result. There was such a huge taboo around using calculator growing up (Asian anyone?), I had my teacher twice when we were formally allowed to use a scientific calculator. I cherished mine for years. This is of course not as smart nor scientific as the CASIO one I had. 
+
+The hardest part was building the logic to recall which operator was previously clicked between the two numbers so that the two numbers can be run through a calculation. 
+
+Some theoretical ideas I entertained: 
+- Can I capture and store whatever events take place after one operator is clicked until the next one is clicked? Like a bubble that is initiated by one event and terminated by another event?
+- Can I ask the computer to wait for the next number to be input after an operator is clicked? What if I ask politely?
+
+I didn't even know how to go about phrasing google search words to explore those ideas, and lying in bed after midnight looking down the imminent cliff of confusion, I had to keep telling myself, the simpler the better. Occum's razor. If it feels like overcomplication, it may just well be. 
+
+After a couple of nights seeing minimal progress other than tweaking display functions, etc, the lightbulb finally went on. And it was a simple solution. Variables. 
+
+It wasn't exactly 100% smooth sailing afterwards, but definitely gained momentum. 
+
+For the future me, in case I ever decide to return for a version update, see if you can add the feature to allow keyboard input of numbers. 
---
+++ script.js
@@ -0,0 +1,200 @@
+//Mathematical functions//
+function add(x,y) {
+    return sum = x+y;
+}
+
+function subtract(x,y) {
+    return difference = x-y;
+}
+
+function multiply(x,y) {
+    return product = Math.round(x*y * 100)/100;
+}
+
+function divide(x,y) {
+    if (y === 0) result = 'Nope';
+    else result = Math.round(x/y * 100)/100;
+    return result
+}
+
+function calculate(operatorFunc, x, y) {
+    return operatorFunc(x,y)
+}
+
+//NUMERIC BUTTONS//
+//listen for 'click', access the value of the button clicked, and display in the display-container//
+//numbers should be able to be concatenated if desired//
+const display = document.querySelector('#display-container')
+const buttonsNum = Array.from(document.querySelectorAll('button.input'));
+
+buttonsNum.forEach(button => button.addEventListener('click', inputNumber));
+
+function inputNumber() {
+    clearResult();
+    const numInput = this.textContent;
+    const addNum = document.createElement('p');
+    addNum.textContent = `${numInput}`;
+    addNum.classList.add('number');
+    display.appendChild(addNum);
+}
+
+//CLEAR BUTTON//
+//listen for 'click', clear the display, initialize running result to zero and empty out the twoNumbers array//
+const buttonClear = document.querySelector('button.clear');
+buttonClear.addEventListener('click', () => {
+    while (display.hasChildNodes()) display.removeChild(display.firstChild);
+    runningResult = 0;
+    twoNumbers.splice(0, twoNumbers.length);
+    previousOperater = undefined;
+});
+
+//+/- BUTTON//
+//listen for 'click', toggle a negative sign in front of the input number//
+const buttonPlusMinus = document.querySelector('.plus-minus');
+buttonPlusMinus.addEventListener('click', toggleNeg);
+
+function toggleNeg() {
+    const negSign = document.createElement('p');
+    negSign.textContent='-';
+    negSign.classList.add('negsign');
+    if (display.firstChild.className === 'negsign') {
+        display.removeChild(display.firstChild);
+    } else {
+    display.insertBefore(negSign, display.firstChild);
+    }
+}
+
+//DELETE button//
+//listen for 'click', remove the last digit from display//
+const buttonDelete = document.querySelector('.delete');
+buttonDelete.addEventListener('click', () => {
+    display.removeChild(display.lastChild);
+})
+
+//'.' button//
+//listen for 'click', add .//
+const buttonDot = document.querySelector('.dot');
+buttonDot.addEventListener('click', () => {
+    const dot = document.createElement('p');
+    dot.textContent = '.';
+    display.appendChild(dot);
+})
+
+//for dealing with display
+
+function clearDisplay() {
+    while (display.hasChildNodes()) display.removeChild(display.firstChild);
+}
+
+function getNumber() {
+    const displayChild = Array.from(display.querySelectorAll('.number'));
+    const numValue = displayChild.map(child => child.textContent);
+    const arrayToNum = numValue.reduce((previousNum, nextNum) => {
+        return previousNum + nextNum;
+    }, 0)
+    return Number(arrayToNum)
+}
+
+function displayResult() {
+    const addResult = document.createElement('p');
+    addResult.textContent = `${runningResult}`;
+    addResult.classList.add('result');
+    display.appendChild(addResult);
+}
+
+function clearResult() {
+    const result = display.querySelector('.result');
+    if (result) result.remove();
+}
+
+//OPERATOR button//
+//When operator button is clicked, save the number entered before the operator button in the array
+//if this is the first operator clicked, then save the operator so that it can be executed when (=) clicked
+//if this is the successive operator, then calculate the result using the previous operator and reassign the
+//operator as the current operator clicked 
+//also, when operator clicked, style changes (colors invert)
+
+let twoNumbers = [];
+let runningResult = 0;
+let previousOperator;
+
+function operatorClicked(currentOperator) {
+    if (previousOperator === undefined) {
+        previousOperator = currentOperator;
+        twoNumbers.push(getNumber());
+        return runningResult = twoNumbers[0];
+    } else { 
+        twoNumbers.push(getNumber()); 
+        runningResult = calculate(previousOperator, twoNumbers[0], twoNumbers[1]);
+        twoNumbers.splice(0, 2);
+        twoNumbers.push(runningResult);
+        previousOperator = currentOperator;
+        return runningResult;
+    }
+}
+
+function buttonActive() {
+    const operatorButtons = document.querySelectorAll('.right button');
+    operatorButtons.forEach(button => button.classList.add('active'));
+}
+
+function buttonInactive() {
+    const activeButton = document.querySelector('.active');
+    if (activeButton) activeButton.classList.remove('active');
+}
+
+//PLUS button//
+const buttonPlus = document.querySelector('#add');
+
+function whenOperatorClicked(operator) {
+    operatorClicked(operator);
+    clearDisplay();
+    displayResult();
+    buttonInactive();
+    buttonActive();
+}
+
+buttonPlus.addEventListener('click', function() {
+    whenOperatorClicked(add);
+});
+
+//MINUS button//
+const buttonMinus = document.querySelector('#subtract');
+buttonMinus.addEventListener('click', function() {
+    whenOperatorClicked(subtract);
+})
+
+//MULTIPLY button//
+const buttonMultiply = document.querySelector('#multiply');
+buttonMultiply.addEventListener('click', function() {
+    whenOperatorClicked(multiply)
+})
+
+//DIVIDE button//
+const buttonDivide = document.querySelector('#divide');
+buttonDivide.addEventListener('click', function() {
+    whenOperatorClicked(divide)
+})
+
+//EQUALS button//
+const buttonEquals = document.querySelector('#equals');
+buttonEquals.addEventListener('click', function() {
+    twoNumbers.push(getNumber());
+    runningResult = calculate(previousOperator, twoNumbers[0], twoNumbers[1]);
+    clearDisplay();
+    displayResult();
+    twoNumbers.splice(0, 2);
+    previousOperator = undefined;
+    toggleClassResult();
+});
+
+function toggleClassResult() {
+    const result = document.querySelector('.result');
+    result.classList.toggle('number');
+}
+
+//footer - insert current year//
+const footer = document.querySelector('#footer');
+const currentYear = document.querySelector('#currentyear');
+currentYear.textContent = `${(new Date().getFullYear())}`;
+footer.appendChild(currentYear);
